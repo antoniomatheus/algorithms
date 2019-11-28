e7cb69904f238e153805097810592962a877c38e
Antonio Matheus Silva <antonimathsilva@gmail.com>
2019-11-28 15:56:11 -0300

    graph traversal, bfs and dfs

--- data-structures/Graph.js
+++ data-structures/Graph.js
@@ -53,4 +53,87 @@ class Graph {
 
     return this;
   }
+
+  DFSRecursive(start) {
+    const result = [];
+    const visited = {};
+    (function iter(vertex, adjacencyList) {
+      if (!adjacencyList[vertex]) {
+        return;
+      }
+      result.push(vertex);
+      visited[vertex] = true;
+      for (let edge of adjacencyList[vertex]) {
+        if (!visited[edge]) {
+          iter(edge, adjacencyList);
+        }
+      }
+    })(start, this.adjacencyList);
+    return result;
+  }
+
+  DFSIter(start) {
+    const stack = [start];
+    const alreadyStacked = {
+      [start]: true
+    };
+    const result = [];
+    while (stack.length) {
+      const vertex = stack.pop();
+      if (!this.adjacencyList[vertex]) {
+        return;
+      }
+      result.push(vertex);
+      for (const edge of this.adjacencyList[vertex]) {
+        if (!alreadyStacked[edge]) {
+          stack.push(edge);
+          alreadyStacked[edge] = true;
+        }
+      }
+    }
+    return result;
+  }
+
+  BFS(start) {
+    const queue = [start];
+    const result = [];
+    const alreadyQueued = {
+      [start]: true
+    };
+    while (queue.length) {
+      const vertex = queue.shift();
+      if (!this.adjacencyList[vertex]) return;
+      result.push(vertex);
+      for (const edge of this.adjacencyList[vertex]) {
+        if (!alreadyQueued[edge]) {
+          alreadyQueued[edge] = true;
+          queue.push(edge);
+        }
+      }
+    }
+    return result;
+  }
 }
+
+module.exports = Graph;
+
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+
+console.log(graph.DFSRecursive("A"));
+console.log(graph.DFSIter("A"));
+console.log(graph.BFS("A"));
